42dd64a8c9b0509a255c27018585297634e5d470
Jacky <29943110+jw-12138@users.noreply.github.com>
2024-07-12 12:27:58 +0800

    feat: add cursor for bucket list

--- src/routes/patch.ts
+++ src/routes/patch.ts
@@ -4,7 +4,11 @@ interface Env {
   R2_BUCKET: R2Bucket;
 }
 
-export default async function(c: Context){
-  let list = await c.env.R2_BUCKET.list()
+export default async function (c: Context) {
+  let cursor = c.req.query('cursor')
+  let list = await c.env.R2_BUCKET.list({
+    cursor: cursor || undefined
+  })
+
   return c.json(list)
 }
